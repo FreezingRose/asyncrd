9ef3443f07a553f3d8b0a9b774e47c1eb8aaeaf9
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-22 21:38:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="asyncrd",
-    version="2.1.6",
+    version="2.1.7",
     author="Alex Hutz",
     author_email="frostiitheweeb@outlook.com",
     description="A small project and a wrapper for Redis.",
