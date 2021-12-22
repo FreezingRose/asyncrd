f4c28381dcc1332f647a6dbb4668ae7230959925
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-22 20:10:05 +0100

    Update query.py

--- asyncrd/query.py
+++ asyncrd/query.py
@@ -18,8 +18,8 @@ class Query():
         self.writer = connection.writer
         
     async def do_query(self, protocol : typing.Union[Get, Set]):
-        self.writer.write(protocol.query.encode())
+        self.writer.write(protocol.query+"\r\n")
         await self.writer.drain()
-        data = await self.reader.read(4096)
+        data = await self.reader.read(100)
         return Result(f'{data.decode()!r}')
     
