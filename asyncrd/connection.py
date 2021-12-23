5c0dcc3fbffd0751893afc4279cda1ed089561fc
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-12-24 00:34:08 +0700

    Update connection.py

--- asyncrd/connection.py
+++ asyncrd/connection.py
@@ -9,12 +9,28 @@ class ConnectionProtocol():
         self.hostname = self.connection_url.hostname
         self.port = self.connection_url.port
         
+        self.reader = None
+        self.writer = None
+        
+        self._closed = False
+        
     async def connect(self):
         self.reader, self.writer = await asyncio.open_connection(self.hostname, self.port)
         
     async def close(self):
+        if self._closed:
+            raise RedisException('Connection is closed.')
+            
         self.writer.close()
         await self.writer.wait_closed()
+        self._closed = True
+        
+    async def __aenter__(self):
+        await self.connect()
+        return self
+    
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
         
     async def get(self, query : str):
         data = Query(self)
