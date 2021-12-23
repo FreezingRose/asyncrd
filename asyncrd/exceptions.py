eed2705bc54c1b3060c8158d172fa30e00a3e9e2
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-23 09:41:47 +0100

    Update exceptions.py

--- asyncrd/exceptions.py
+++ asyncrd/exceptions.py
@@ -1,7 +1,15 @@
 import asyncio, typing
 import aioconsole
 
+class RedisOK():
+    def __init__(self, message : str):
+        self.msg = message
+
 class RedisException(Exception):
+    def __init__(self, message : str):
+        super().__init__(message)
+        
+class RedisCommandUnknown(Exception):
     def __init__(self, message : str):
         super().__init__(message)
 
@@ -13,8 +21,15 @@ class CatchException():
     async def catch_error(self):
         if self.text.startswith("-ERR"):
             text = self.text.split("-ERR ")
+            if "unknown command in " in text[1]:
+                text_one = text[1]
+                raise RedisCommandUnknown(text_one)
+                return
             raise RedisException(text[1])
             return
+        if self.text.startswith("+OK"):
+            res = RedisOK("OK")
+            return res.message
         return self.text
             
         
