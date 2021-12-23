aefd39b89d6436d9dc25f9ae1c2034d2ed06d697
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-23 19:00:21 +0100

    Update exceptions.py

--- asyncrd/exceptions.py
+++ asyncrd/exceptions.py
@@ -1,5 +1,6 @@
 import asyncio, typing
 import aioconsole
+from .parser import Parser
 
 class RedisOK():
     def __init__(self, message : str):
@@ -26,28 +27,8 @@ class CatchException():
         self.text = text
         
     async def catch_error(self):
-        if self.text.startswith("-ERR") or self.text.startswith("-WRONGTYPE "):
-            text = self.text.split("-ERR ")
-            if "unknown" in text[1]:
-                text_one = text[1]
-                raise RedisCommandUnknown(text_one)
-                return
-            if "Operation against a key holding the wrong kind of value" in text[1]:
-                text_one = text[1]
-                raise RedisWrongType(text_one)
-                return               
-            raise RedisException(text[1])
-            return
-        if self.text.startswith("+OK"):
-            res = RedisOK("OK")
-            return res.msg
-        self.text = self.text.strip("$5")
-        self.text = self.text.strip("$6")
-        try:
-            if int(self.text) == -1:
-                self.text = []
-        except:
-            pass
-        return self.text
+        parser = Parser()
+        res = await parser.decode(self.text)
+        return res
             
         
