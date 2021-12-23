a33d2d8fd4b7d1069ae2adaa62073d789af8560c
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-23 12:25:37 +0100

    Update exceptions.py

--- asyncrd/exceptions.py
+++ asyncrd/exceptions.py
@@ -12,6 +12,10 @@ class RedisException(Exception):
 class RedisCommandUnknown(Exception):
     def __init__(self, message : str):
         super().__init__(message)
+        
+class RedisWrongType(Exception):
+    def __init__(self, message : str):
+        super().__init__(message)
 
 
 class CatchException():
@@ -19,12 +23,16 @@ class CatchException():
         self.text = text
         
     async def catch_error(self):
-        if self.text.startswith("-ERR"):
+        if self.text.startswith("-ERR") or self.text.starswith("-WRONGTYPE "):
             text = self.text.split("-ERR ")
             if "unknown" in text[1]:
                 text_one = text[1]
                 raise RedisCommandUnknown(text_one)
                 return
+            if "Operation against a key holding the wrong kind of value" in text[1]:
+                text_one = text[1]
+                raise RedisWrongType(text_one)
+                return               
             raise RedisException(text[1])
             return
         if self.text.startswith("+OK"):
