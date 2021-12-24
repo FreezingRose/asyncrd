0075832595158d0eec57ac7a2dd143a124b5a10b
IceRose <95206604+FreezingRose@users.noreply.github.com>
2021-12-25 00:25:57 +0530

    Update __init__.py

--- asyncrd/__init__.py
+++ asyncrd/__init__.py
@@ -1,7 +1,7 @@
 from .connection import *
 from .query import *
 
-class connecter():
+class Connector:
     def __init__(self, connection_url : str):
         self.connection_url = connection_url
         self.connection = ConnectionProtocol(connection_url)
@@ -14,4 +14,7 @@ class connecter():
         await self.connection.close()
         return self
 
-connect = connecter()
+async def connect(url):
+    connection = Connector(url)
+    return connection
+     
