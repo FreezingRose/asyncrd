586a5a8488ef394a8f8fa1b80b5636c0a8b2e6bf
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-24 20:04:30 +0100

    Update __init__.py

--- asyncrd/__init__.py
+++ asyncrd/__init__.py
@@ -3,5 +3,6 @@ from .query import *
 
 async def connect(connection_url):
     connection = ConnectionProtocol(connection_url)
+    connection = await connection.connect()
     cls = connection
     return cls
