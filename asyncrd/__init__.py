6c9e879b56d942ad39dc954cb38ba76e5337219e
FrostiiWeeb <73381596+FrostiiWeeb@users.noreply.github.com>
2021-12-24 19:48:34 +0100

    Update __init__.py

--- asyncrd/__init__.py
+++ asyncrd/__init__.py
@@ -1,9 +1,17 @@
 from .connection import *
 from .query import *
-from contextlib import asynccontextmanager
 
-@asynccontextmanager
-async def connect(connection_url : str):
-    CONNECTION = ConnectionProtocol(connection_url)
-    await CONNECTION.connect()
-    return CONNECTION
+class connect():
+    def __init__(self, connection_url : str):
+        self.connection_url = connection_url
+        self.connection = ConnectionProtocol(connection_url)
+        
+    async def __aenter__(self):
+        await self.connection.connect()
+        return self.connection
+    
+    async def __aexit__(self):
+        await self.connection.close()
+        return self
+
+connect = connect
